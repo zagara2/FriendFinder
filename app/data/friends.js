48f62e820ad4a64d26dd8ca82a064ab5849a27bc
Mickey S <marimikaels@gmail.com>
2017-05-18 02:19:40 -0400

    comparison logic for scores, put a default friend in the friends api

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var userData = [{
+name: "Ahmed",
+photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+scores: [
+"5",
+"1",
+"4",
+"4",
+"5",
+"1",
+"2",
+"5",
+"4",
+"1"
+]}
+];
+
+module.exports = userData;
